6a6c80b87e881139733060b4697bb9d57931e9f5
Ram Gopal Siddh <ramgopalsiddh.000@gmail.com>
2024-01-25 22:14:46 +0530

    DAY 1 print predfine input and manuplate string

---
+++ days1/1_hello.rb
@@ -0,0 +1,16 @@
+# print hello world in differnt ways
+
+print "Hello World"
+
+puts "Hello World"
+
+p "Hello World"
+
+# greeting "Hello World"
+# puts greeting
+
+def say_hello(thing_to_say)
+    puts thing_to_say
+end
+
+say_hello "Hello World Ruby is great!"
---
+++ days1/2_string.rb
@@ -0,0 +1,22 @@
+# string concatenation 
+   # In this don't have space between 2 strings when join it put " " OR ' ' for space 
+first_name = "Ram Gopal"
+last_name = "Siddh"
+puts first_name + last_name 
+
+# string interpolation
+    # This works only in " "(Double cotes) don't work in single cotes
+puts "My first name is #{first_name} and last name is #{last_name}"
+
+
+
+# methods, how to find them  
+
+  # .methods and find methods and use there methods for change in given stirngs and 
+ # also can change data type by useing .methods and also change nature of given string's
+
+# common methods
+
+# variable assignment 
+
+# Escaping
